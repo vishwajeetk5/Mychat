5dc6c5d0e0fce1ff2c94cdb9cf96a38e8dee5af0
Vishwajeet Koshti <vishwajeetskoshti2003@gmail.com>
2025-02-07 18:38:50 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // const socket = io('http://192.168.0.109:3000',{})
-const socket = io('http://localhost:3000',{})
+const socket = io('https://mychat-production-35b7.up.railway.app/',{})
 
 const totClients = document.getElementById('client-total');
 
